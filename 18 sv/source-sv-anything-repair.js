7c994df192d6c90ddd7a6be0e12fedc82d0706c1
msuzuki <led.centipede@gmail.com>
2017-03-11 02:21:54 +0900

    対象ソース修正ソースを書き換えるbkmkテスト

---
+++ 18 sv/source-sv-anything-repair.js
@@ -0,0 +1,115 @@
+javascript:(function(){
+	function surveyDialogUtil() {
+		form = document.forms["diag_form"];
+		comment = document.getElementById("comments");
+		description = document.getElementById("description");
+		srccode = document.getElementById("srccode");
+		hash = {
+			"PASS":"適合",
+			"PASS2":"適合(注記)",
+			"FAIL":"不適合",
+			"FAIL2":"不適合(要再判定)",
+			"NA":"非適用"
+		};
+		cp_hash = {
+			"yes":"1",
+			"no":"0",
+		};
+	}
+	surveyDialogUtil.prototype = {
+		get_survey: function() {
+			var key = "";
+			for(var i=0; i<form.elements.length; i++) {
+				var ip = form.elements[i];
+				if(ip.checked==true) {
+					key = ip.value;
+					break;
+				}
+			}
+			if(key!=="") return hash[key];
+			else return "";
+		},
+		get_comment: function() {
+			return comment.value;
+		},
+		get_description: function() {
+			return description.value;
+		},
+		get_srccode: function() {
+			return srccode.value;
+		},
+		set_survey: function(flag) {
+			for(var i=0; i<form.elements.length; i++) {
+				var ip = form.elements[i];
+				var val = ip.value;
+				if(val===flag) {
+					this.diag_clean(flag);
+					ip.click();
+					break;
+				}
+			}
+		},
+		set_comment: function(str) {
+			comment.value = str;
+		},
+		set_description: function(str) {
+			description.value = str;
+		},
+		set_srccode: function(str) {
+			srccode.value = str;
+		},
+		set_survey_copy: function(flag) {
+			var key = cp_hash[flag];
+			var rds = document.getElementsByName("copy");
+			if(rds !== "undefined") {
+				for(var i=0; i<rds.length; i++) {
+					var val = rds[i].value;
+					if(key === val) {
+						rds[i].click();
+						break;
+					}
+				}
+			}
+		},
+		save: function() {
+			for(var i=0; i<form.elements.length; i++) {
+				var ip = form.elements[i];
+				var tp = ip.type;
+				if(tp==="submit") {
+					ip.click();
+					break;
+				}
+			}
+		},
+		text_clean: function(str) {
+		    str=str.replace(/^ +/m,"");
+		    str=str.replace(/\t+/m,"");
+		    str=str.replace(/(\r\n|\r|\n)/g,""); 
+		    return str;
+		},
+		diag_clean: function(flag) {
+			switch(flag) {
+				case "PASS":
+					this.set_comment("");
+					this.set_srccode("");
+					break;
+				case "NA":
+					this.set_comment("");
+					this.set_description("");
+					this.set_srccode("");
+					break;
+			}
+		}
+	};
+
+	var util = new surveyDialogUtil();
+	var p1 = new RegExp(/(<span class="help_text">)(.+?)(<\/span>)/g);
+    var p2 = new RegExp(/(<p><span class="help_text">)(.+?)(<\/span><\/p>)/g);
+	var desc = util.text_clean(util.get_description());
+	var src = util.text_clean(util.get_srccode());
+	desc = desc.replace(p1, "");
+	src = src.replace(p2, "");
+	util.set_description(desc);
+	util.set_srccode(src);
+
+})();
