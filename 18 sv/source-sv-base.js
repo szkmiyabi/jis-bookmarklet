84c2c33956742b27fc2e129353ca4001b700f67d
msuzuki <led.centipede@gmail.com>
2017-10-16 03:07:04 +0900

    個別検査基底クラスの修正

--- 18 sv/source-sv-base.js
+++ 18 sv/source-sv-base.js
@@ -12,8 +12,8 @@ javascript:(function(){
 			"NA":"非適用"
 		};
 		cp_hash = {
-			"yes":"1",
-			"no":"0",
+			"1":"yes",
+			"0":"no",
 		};
 	}
 	surveyDialogUtil.prototype = {
@@ -27,6 +27,20 @@ javascript:(function(){
 				}
 			}
 			if(key!=="") return hash[key];
+			else return "";
+		},
+		get_survey_cp: function() {
+			var key = "";
+			var rdos = document.getElementsByName("copy");
+			if(rdos === "undefined") return "";
+			for(var i=0; i<rdos.length; i++) {
+				var rdo = rdos.item(i);
+				if(rdo.checked==true) {
+					key = rdo.value;
+					break;
+				}
+			}
+			if(key!=="") return cp_hash[key];
 			else return "";
 		},
 		get_comment: function() {
@@ -59,12 +73,11 @@ javascript:(function(){
 			srccode.value = str;
 		},
 		set_survey_copy: function(flag) {
-			var key = cp_hash[flag];
 			var rds = document.getElementsByName("copy");
 			if(rds !== "undefined") {
 				for(var i=0; i<rds.length; i++) {
 					var val = rds[i].value;
-					if(key === val) {
+					if(val===flag) {
 						rds[i].click();
 						break;
 					}
@@ -99,8 +112,59 @@ javascript:(function(){
 					this.set_srccode("");
 					break;
 			}
+		},
+		get_survey_key: function(flag_word) {
+			var ret_key = "";
+			for(var key in hash) {
+				var val = hash[key];
+				if(val === flag_word) {
+					ret_key = key;
+					break;
+				}
+			}
+			return ret_key;
+		},
+		get_survey_cp_key: function(flag_word) {
+			var ret_key = "";
+			for(var key in cp_hash) {
+				var val = cp_hash[key];
+				if(val === flag_word) {
+					ret_key = key;
+					break;
+				}
+			}
+			return ret_key;
+		},
+		is_survey_page: function() {
+			var pat1 = new RegExp(/\/diagnose\/indexv2\/index\/projID\/\"*[0-9]+\"*/);
+			var pat2 = new RegExp(/\/diagnose\/indexv2\/index\/reset\/(true|false)\/projID\/\"*[0-9]+\"*/);
+			if(pat1.test(url) || pat2.test(url)) return true;
+			else return false;
+		},
+		is_main_page: function() {
+			var pat = new RegExp(/\/diagnose\/indexv2\/report\/projID\/[0-9]+/);
+			if(pat.test(url)) return true;
+			else return false;
+		},
+		is_detail_page: function() {
+			var pat = new RegExp(/\/diagnose\/indexv2\/report2\/projID\/[0-9]+\/controlID\//);
+			if(pat.test(url)) return true;
+			else return false;
+		},
+		get_tech: function() {
+			var tech = "";
+			var techList = document.getElementById("techList");
+			tech = techList.options[techList.selectedIndex].value;
+			tech = tech.replace(/【完】/,"");
+			return tech;
+		},
+		get_safe_value: function(val) {
+			if(typeof val === "undefined") return "";
+			else return val;
 		}
 	};
+
 	var util = new surveyDialogUtil();
 	util.set_survey_copy("no");
+	
 })();
